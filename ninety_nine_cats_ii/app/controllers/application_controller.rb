ba1dc0fe74a3b286eb305d9c8ee6662037818b38
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-14 17:30:52 -0800

    Add login and logout buttons across web pages

--- ninety_nine_cats_ii/app/controllers/application_controller.rb
+++ ninety_nine_cats_ii/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+  def logout!
+    self.current_user.reset_session_token! if self.current_user
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 end
--- ninety_nine_cats_ii/app/controllers/sessions_controller.rb
+++ ninety_nine_cats_ii/app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by_credentials(
+      params[:user][:user_name],
+      params[:user][:password]
+    )
+    if user
+      login!(user)
+      redirect_to cats_url
+    else
+      render json: "bad credentials"
+    end
   end
 
   def destroy
-
+    logout!
+    redirect_to cats_url
   end
 end
